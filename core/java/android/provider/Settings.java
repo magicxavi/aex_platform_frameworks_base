5e1ba21386fab32c6937afdab88f7eb5cfa319e7
Pranav Vashi <neobuddy89@gmail.com>
2018-04-02 00:10:08 +0530

    Add toggle to enable Android P system animation [1/2]
    Change-Id: Iabef2773c54aee04bea34ed42b04b14cb6c65aa1

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11119,6 +11119,12 @@ public final class Settings {
         public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
 
         /**
+         * Default animation
+         * @hide
+         */
+        public static final String SYSTEM_DEFAULT_ANIMATION = "system_default_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
